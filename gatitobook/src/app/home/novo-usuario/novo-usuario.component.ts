41fcea22483730c094a4b760c4ecc5bd587ce439
alencartha <alencar_tha@hotmail.com>
2021-05-26 16:22:38 -0300

    validacoes campos cadastro

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,6 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 
 @Component({
@@ -18,15 +18,15 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-        email:[''],
-        fullName:[''],
-        userName:[''],
-        password:['']
-    })
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: [''],
+      password: [''],
+    });
   }
 
-  cadastrar(){
+  cadastrar() {
     const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario)
+    console.log(novoUsuario);
   }
 }
